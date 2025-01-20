bbfd90267835a59877558941a804dfd841023405
Massi Saoudi <106273388+Maci96@users.noreply.github.com>
2025-01-20 14:37:24 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Print SECRET_ENV environment variable."""
 import os
-import sys
-
+import logging
 from dotenv import load_dotenv
 
-
 def main():
-    """Run administrative tasks."""
-    # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running on Azure App Service
+    """Log SECRET_ENV variable."""
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-    # Only for Local Development - Load environment variables from the .env file
-    if 'WEBSITE_HOSTNAME' not in os.environ:
-        print("Loading environment variables for .env file")
-        load_dotenv('./.env')
+    # Load environment variables from the .env file
+    load_dotenv('./.env')
 
-    # When running on Azure App Service you should use the production settings.
-    settings_module = "azureproject.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azureproject.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+    # Log the SECRET_ENV variable if it exists
+    secret_env = os.getenv('SECRET_ENV')
+    if secret_env:
+        logging.info(f"SECRET_ENV: {secret_env}")
+    else:
+        logging.info("SECRET_ENV is not set")
 
 if __name__ == '__main__':
     main()
